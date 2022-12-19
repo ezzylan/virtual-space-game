3327430c9229ce7f7d87700a3769c297f94b0533
sitisarahaqilah <sarahhishammuddin@gmail.com>
2022-12-19 11:34:58 +0800

    base scenes

---
+++ src/App.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        Parent root;
+
+        try {
+            root = FXMLLoader.load(getClass().getResource("IndexScene.fxml"));
+            Scene scene = new Scene(root);
+            primaryStage.setTitle("Virtual Space Game");
+            primaryStage.setScene(scene);
+            primaryStage.setResizable(false);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
