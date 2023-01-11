c5422af7583edad098ae732a7d4e9c91ee3d2e15
aisyahatirah <fancyonce21@gmail.com>
2023-01-11 13:58:58 +0800

    base

---
+++ src/MenuItem.java
@@ -0,0 +1,24 @@
+public class MenuItem {
+    String name;
+    double price;
+    String description;
+
+    // a constructor to create menu item
+    public MenuItem(String name, double price, String description) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
--- src/ViewMenuController.java
+++ src/ViewMenuController.java
@@ -1,3 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class ViewMenuController {
     
+    List<MenuItem> menuItems;
+
+    public ViewMenuController(){
+        menuItems = new ArrayList<MenuItem>();
+
+        addItem("Pasta", 12.0, "Simple and delicious, with house-made tomato-basil marinara, drizzled with Tusean olive oil.");
+    }
+
+    public void addItem(String name, double price, String description){
+        MenuItem menuItem = new MenuItem(name, price, description);
+        menuItems.add(menuItem);
+    }
+
+    public ArrayList<MenuItem> getMenuItems(){
+        return (ArrayList<MenuItem>) menuItems;
+    }
 }
