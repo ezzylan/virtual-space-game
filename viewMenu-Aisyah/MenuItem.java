9b4d8e631eff1d2db225f5f8db982d7ac4f5a2a3
aisyahatirah <fancyonce21@gmail.com>
2023-01-14 22:02:17 +0800

    for pull reuqest purpose

---
+++ viewMenu-Aisyah/MenuItem.java
@@ -0,0 +1,24 @@
+//template method for each menu item
+public abstract class MenuItem {
+
+    public abstract String getMenuName();
+
+    public abstract String getMenuImage();
+
+    public abstract String getMenuDesc();
+
+    public abstract Double getMenuPrice();
+
+    public void addItemToCart() {
+        System.out.println("Add item to cart");
+    }
+
+    public void removeItemFromCart() {
+        System.out.println("Remove item from cart");
+    }
+
+    public void countTotal() {
+        System.out.println("Total:");
+    }
+
+}
